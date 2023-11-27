f6b0e891d61ab147311fd8c7a40cc431371a5701
DJATIO TCHOTEZO Stéphane <stephanedjatio@gmail.com>
2023-11-27 03:39:21 +0100

    Add files via upload

--- backend/urls.py
+++ backend/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.conf.urls.static import static, serve
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.conf import settings
@@ -26,8 +26,15 @@ urlpatterns = [
     path('api/', include('api.urls')),
 ]
 
+
 # Serving the media files in development mode
 # if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # else:
-# urlpatterns += staticfiles_urlpatterns()
+#     urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
